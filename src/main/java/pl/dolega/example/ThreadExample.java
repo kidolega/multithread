c30b275976e142d5e122d5747eada14d2bcb2f87
Marcin Dolega <marcin_dolega@epam.com>
2022-09-16 13:21:39 +0200

    ExecutorService Example

--- src/main/java/pl/dolega/example/ThreadExample.java
+++ src/main/java/pl/dolega/example/ThreadExample.java
@@ -1,33 +1,25 @@
 package pl.dolega.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadExample {
 
     public static void main(String[] args) {
 
-        Thread t1 = new Thread(new Runnable() {
-            public void run() {
-                for (int i = 1; i < 1001; i++) {
-                    System.out.println(Thread.currentThread().getName() + " " + i);
-                }
-            }
-        });
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        Thread t2 = new Thread(() -> {
-            for (int i = 1; i < 1001; i++) {
-                System.out.println(Thread.currentThread().getName() + " " + i);
-            }
-        });
+        executorService.submit(ThreadExample::loop);
+        executorService.submit(ThreadExample::loop);
+//        executorService.submit(ThreadExample::loop);
+//        executorService.shutdownNow();
+        executorService.shutdown();
 
-        t1.setPriority(1);
-        t2.setPriority(10);
+    }
 
-//        t1.setName("t1");
-//        t2.setName("t2");
-
-        t1.start();
-        t2.start();
-//        t1.run();
-//        t2.run();
-
+    private static void loop() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
     }
 }
